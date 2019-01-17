66840582beae2be0aaf7a306d3269fd813ac5d54
Deepanshu Manocha <deepanshu18csu096@ncuindia.edu>
2019-01-17 09:51:20 +0530

    FOCP2

---
+++ FOCP2/ControlStatements/src/digit.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class digit
+{
+	public static void input(int [] arr)
+	{
+		Scanner sc= new Scanner (System.in);
+		System.out.print("Enter Value: ");
+		arr[x]=sc.nextInt();
+	}
+	public static void main(String [] args) 
+	{
+
+	}
+}
